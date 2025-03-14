5cdcce89c49d06040b1716ffaee8d8ec807cb112
Prakhar Agrawal <prakharagrawal2697@gmail.com>
2025-03-14 17:36:44 +0530

    Updated Data Ingestion module

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+from src.exception import CustomException
+from src.logger import logging
+
+@dataclass
+class DataIngestionConfig:
+    """
+    Configuration class for data ingestion paths.
+    """
+    train_data_path: str = os.path.join('artifacts', "train.csv")
+    test_data_path: str = os.path.join('artifacts', "test.csv")
+    raw_data_path: str = os.path.join('artifacts', "data.csv")
+
+class DataIngestion:
+    """
+    Class for handling data ingestion.
+    """
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        """
+        Initiates the data ingestion process.
+        Reads the dataset, splits it into train and test sets, and saves them to the specified paths.
+        """
+        logging.info("Starting data ingestion process...")
+        try:
+            # Read the dataset
+            df = pd.read_csv('notebook/data/stud.csv')
+            logging.info('Read the dataset as a DataFrame')
+
+            # Create the artifacts directory if it doesn't exist
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+            # Save the raw data
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            logging.info("Raw data saved to artifacts directory")
+
+            # Split the data into train and test sets
+            logging.info("Train-test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            # Save the train and test sets
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            logging.info("Train and test data saved to artifacts directory")
+
+            logging.info("Data ingestion completed successfully")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            logging.error("Error occurred during data ingestion")
+            raise CustomException(e, sys)
